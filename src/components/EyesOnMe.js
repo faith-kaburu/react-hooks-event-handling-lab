32d85e5bf48d4bf042b9e520443132c01d213c27
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-08-04 23:09:42 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe (){
+
+      // Event handler function when the button is focused
+      const handleFocus = () => {
+        console.log('Good!');
+      };
+    
+      // Event handler function when the button is blurred
+      const handleBlur = () => {
+        console.log('Hey! Eyes on me!');
+      };
+    
+      return (
+        <div>
+          {/* Render a button with the text 'Eyes on me' */}
+          <button onFocus={handleFocus} onBlur={handleBlur}>
+            Eyes on me
+          </button>
+        </div>
+      );
+    };
+    
+    export default EyesOnMe;
+    
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
-    return (
-        <div></div>
-    )
-}
+  // Event handler function to handle the change event
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      {/* Render an input field with type "password" */}
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+};
 
 export default Keypad;
